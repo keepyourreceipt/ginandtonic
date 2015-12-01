ed76bbb9ef7931bf465d8921d5d067117c76cd48
Michael <michaelkpeterman@gmail.com>
2015-11-30 20:05:09 -0500

    Begin Woo Commerce integration

--- functions.php
+++ functions.php
@@ -1,16 +1,23 @@
 <?php
 
-function gin_and_tonic_setup() {
+// Add theme support for optional Wordpress features
+function ginandtonic_setup() {
 	add_theme_support( 'menus' );
 }
-add_action( 'after_setup_theme', 'gin_and_tonic_setup' );
+add_action( 'after_setup_theme', 'ginandtonic_setup' );
 
-
-function gin_and_tonic_scripts() {
+// Add theme scripts
+function ginandtonic_scripts() {
 	wp_enqueue_style( 'default-css', get_stylesheet_uri() );
   wp_enqueue_style( 'combined-css', get_template_directory_uri() . '/css/dist/combined.css' );
 
   wp_enqueue_script( 'combined-js', get_template_directory_uri() . '/js/dist/combined.js', array('jquery'), '', true );
 }
 
-add_action( 'wp_enqueue_scripts', 'gin_and_tonic_scripts' );
+add_action( 'wp_enqueue_scripts', 'ginandtonic_scripts' );
+
+// Add Woo Commerce support
+add_action( 'after_setup_theme', 'woocommerce_support' );
+function woocommerce_support() {
+    add_theme_support( 'woocommerce' );
+}
--- page.php
+++ page.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-
-<h2><i class="fa fa-chevron-down"></i></h2>
-
+  <?php while( have_posts() ) : the_post(); ?>
+    <?php the_content(); ?>
+  <?php endwhile; ?>
 <?php get_footer(); ?>
