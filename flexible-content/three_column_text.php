5bd64d9e59444f5cab84f503c967e28ab82af5f4
Michael <michaelkpeterman@gmail.com>
2015-12-28 19:12:42 -0500

    Improve sass file structure. New content row

--- flexible-content/three_column_text.php
+++ flexible-content/three_column_text.php
@@ -3,15 +3,15 @@
     <div class="container">
       <?php
         if( have_rows('text_boxes') ) { ?>
-          <div class="col-sm-4 text-box">
-            <?php while ( have_rows('text_boxes') ) : the_row(); ?>
-                <h2 class="waypoint waypoint-bottom-to-top anim-time-long"><?php the_sub_field('text_heading'); ?></h2>
-                <p><?php the_sub_field('text_content'); ?></p>
-            <?php endwhile; ?>
-          </div>
-          <?php
-          }
-        ?>
+          <?php while ( have_rows('text_boxes') ) : the_row(); ?>
+            <div class="col-sm-4 text-box center-text">
+              <h3 class="waypoint waypoint-bottom-to-top"><?php the_sub_field('text_heading'); ?></h3>
+              <p class="waypoint waypoint-bottom-to-top"><?php the_sub_field('text_content'); ?></p>
+            </div>
+          <?php endwhile; ?>
+        <?php
+        }
+      ?>
     </div>
   </div>
 </div>
