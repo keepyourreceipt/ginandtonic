6dbdc51839d043f7d17d9748d87def3405bfbce1
Michael <michaelkpeterman@gmail.com>
2015-12-30 21:22:56 -0500

    Style changes. Content row edits

--- flexible-content/call_to_action.php
+++ flexible-content/call_to_action.php
@@ -1,12 +1,45 @@
-<div class="call-to-action">
+<?php if( get_sub_field('image_orientation') == "Display image on the left" ) {
+  $image_orientation = "left";
+} else {
+  $image_orientation = "right";
+} ?>
+<div class="hidden-xs hidden-sm call-to-action">
   <?php $featured_image_obj = get_sub_field('featured_image'); ?>
-  <div class="row uniform-col-height">
-    <div class="col-sm-5 hidden-xs background-image waypoint waypoint-left-to-right" style="background-image: url(<?php echo $featured_image_obj['sizes']['full-hd']; ?>);">
-      <?php // Background image container ?>
+  <?php if( $image_orientation == "left" ) { ?>
+    <div class="row uniform-col-height">
+      <div class="col-sm-12 col-md-5 background-image waypoint waypoint-left-to-right" style="background-image: url(<?php echo $featured_image_obj['sizes']['full-hd']; ?>);">
+        <?php // Background image container ?>
+      </div>
+      <div class="col-md-6 waypoint waypoint-right-to-left anim-time-medium">
+        <div class="text-content">
+          <?php the_sub_field('text_editor'); ?>
+        </div>
+      </div>
     </div>
-    <div class="col-md-6 waypoint waypoint-right-to-left anim-time-medium">
-      <div class="text-content">
-        <?php the_sub_field('text_editor'); ?>
+  <?php } else { ?>
+    <div class="row uniform-col-height">
+      <div class="col-sm-offset-1 col-md-6 waypoint waypoint-left-to-right anim-time-medium">
+        <div class="text-content">
+          <?php the_sub_field('text_editor'); ?>
+        </div>
+      </div>
+      <div class="col-sm-5 hidden-xs background-image waypoint waypoint-right-to-left" style="background-image: url(<?php echo $featured_image_obj['sizes']['full-hd']; ?>);">
+        <?php // Background image container ?>
+      </div>
+    </div>
+  <?php } ?>
+</div>
+
+<div class="hidden-md hidden-lg hidden-xl call-to-action-mobile">
+  <div class="jumbotron background-image waypoint waypoint-left-to-right" style="background-image: url(<?php echo $featured_image_obj['sizes']['full-hd']; ?>);">
+    <?php // Background image container ?>
+  </div>
+  <div class="row">
+    <div class="container">
+      <div class="col-sm-12 waypoint waypoint-right-to-left anim-time-medium">
+        <div class="text-content text-center">
+          <?php the_sub_field('text_editor'); ?>
+        </div>
       </div>
     </div>
   </div>
--- flexible-content/content_grid.php
+++ flexible-content/content_grid.php
@@ -1,10 +1,24 @@
-<div class="row">
+<div class="row content-grid">
   <div class="container">
     <?php
       $posts = get_sub_field('content_grid');
       foreach( $posts as $post ) {
-        the_title();
+        ?>
+        <?php setup_postdata($post); ?>
+        <div class="col-sm-4">
+          <a href="<?php the_permalink(); ?>">
+            <?php if( has_post_thumbnail() ) {
+              the_post_thumbnail('post-listing');
+            } ?>
+          </a>
+          <a href="<?php the_permalink(); ?>">
+            <h3><?php the_title(); ?></h3>
+          </a>
+          <p><?php the_excerpt(); ?></p>
+        </div>
+      <?php
       }
     ?>
+    <?php wp_reset_postdata(); ?>
   </div>
 </div>
