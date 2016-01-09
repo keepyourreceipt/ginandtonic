c3a3c26bbec866a707cb210d09431b646406b7cc
Michael <michaelkpeterman@gmail.com>
2016-01-09 12:52:14 -0500

    Add single events page admin message

---
+++ single-events.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+
+<div class="jumbotron">
+  <div class="container">
+      <div class="col-sm-12">
+          <h2>To view an event, please go to the events page. This page is not linked anywhere on your site, only you can see this.</h2>
+      </div>
+  </div>
+</div>
+
+<?php get_footer(); ?>
