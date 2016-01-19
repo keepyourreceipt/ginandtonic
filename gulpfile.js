3227a0e334611a59fa04152714239ddb28f13bf3
Michael <michaelkpeterman@gmail.com>
2016-01-18 22:18:57 -0500

    Remove unused bower components. Cleanup gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,7 @@ var concatCss   = require('gulp-concat-css');
 var cssnano     = require('gulp-cssnano');
 var concat      = require('gulp-concat');
 var uglify      = require('gulp-uglify');
+var replace     = require('gulp-replace');
 
 // Initi live reload server
 livereload({ start: true })
@@ -18,13 +19,28 @@ gulp.task('copy-fonts', function() {
     'bower_components/bootstrap/dist/fonts/**.*',
     'bower_components/components-font-awesome/fonts/**.*'
   ])
-    .pipe(gulp.dest('css/fonts'));
+    .pipe(gulp.dest('fonts'));
 });
 
 // Build CSS
-gulp.task('build-css', ['clean-dist-css', 'compass', 'combine-vendor-css', 'combine-theme-css'], function () {
+gulp.task('build-css', ['compass', 'copy-vendor-images', 'combine-css'], function () {
   // Silence is golden
 });
+
+      gulp.task('combine-css', ['combine-theme-css'], function () {
+        return gulp.src([
+            'bower_components/bootstrap/dist/css/bootstrap.css',
+            'bower_components/components-font-awesome/css/font-awesome.css',
+            'bower_components/slick-carousel/slick/slick.css',
+            'bower_components/photoswipe/dist/photoswipe.css',
+            'bower_components/photoswipe/dist/default-skin/default-skin.css',
+          ])
+          .pipe(replace('../fonts', '../../fonts'))
+          .pipe(concatCss("vendor.min.css", {
+            rebaseUrls: false
+          }))
+          .pipe(gulp.dest('css/dist'));
+      });
 
       gulp.task('combine-theme-css', function () {
         return gulp.src([
@@ -36,20 +52,17 @@ gulp.task('build-css', ['clean-dist-css', 'compass', 'combine-vendor-css', 'comb
           .pipe(gulp.dest('css/dist'));
       });
 
-      gulp.task('combine-vendor-css', function () {
+      gulp.task('copy-vendor-images', function() {
         return gulp.src([
-            'bower_components/bootstrap/dist/css/bootstrap.css',
-            'bower_components/components-font-awesome/css/font-awesome.css',
-            'bower_components/slick-carousel/slick/slick.css',
-            'bower_components/featherlight/src/featherlight.css',
-          ])
-          .pipe(concatCss("vendor.min.css", {
-            rebaseUrls: false
-          }))
-          .pipe(gulp.dest('css/dist/'));
+          'bower_components/photoswipe/dist/default-skin/default-skin.png',
+          'bower_components/photoswipe/dist/default-skin/default-skin.svg',
+          'bower_components/photoswipe/dist/default-skin/preloader.gif',
+        ])
+        .pipe( gulp.dest('css/dist') )
+
       });
 
-      gulp.task('compass', function() {
+      gulp.task('compass', ['clean-dist-css'], function() {
         return gulp.src('sass/*.sass')
           .pipe(compass({
             config_file: 'config.rb',
@@ -60,7 +73,7 @@ gulp.task('build-css', ['clean-dist-css', 'compass', 'combine-vendor-css', 'comb
       });
 
       gulp.task('clean-dist-css', function () {
-      	return gulp.src('css/dist/*', {read: false})
+      	return gulp.src('css/dist/*.css', {read: false})
       		.pipe(clean());
       });
 
@@ -83,11 +96,10 @@ gulp.task('build-js', ['clean-dist-js', 'combine-vendor-js', 'combine-theme-js']
           return gulp.src([
             'bower_components/bootstrap/dist/js/bootstrap.js',
             'bower_components/waypoints/lib/jquery.waypoints.js',
-            'bower_components/jquery.stellar/jquery.stellar.js',
             'bower_components/slick-carousel/slick/slick.js',
             'bower_components/fastclick/lib/fastclick.js',
             'bower_components/scrollme/jquery.scrollme.js',
-            'bower_components/featherlight/src/featherlight.js',
+            'bower_components/photoswipe/dist/photoswipe.js',
           ])
           .pipe( concat('vendor.min.js'))
           // .pipe(uglify())
