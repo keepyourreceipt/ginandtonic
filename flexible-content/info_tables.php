4b08379ce608ae888f9d733d5bf0c01da6d11285
Michael <michaelkpeterman@gmail.com>
2016-01-25 19:07:20 -0500

    Finish initial info tables build

--- flexible-content/info_tables.php
+++ flexible-content/info_tables.php
@@ -8,75 +8,81 @@
         $classes = "col-sm-4";
       }
     ?>
-    <table class="<?php echo $classes; ?>">
-      <thead>
-        <tr>
-          <th>
-            <?php the_sub_field('table_one_heading'); ?>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        if( have_rows('table_one_data') ) {
-          while( have_rows('table_one_data') ) : the_row(); ?>
-            <tr>
-              <td>
-                <?php the_sub_field('table_row'); ?>
-              </td>
-            </tr>
+    <div class="<?php echo $classes; ?>">
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <?php the_sub_field('table_one_heading'); ?>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
           <?php
-          endwhile;
-        } ?>
-      </tbody>
-    </table>
+          if( have_rows('table_one_data') ) {
+            while( have_rows('table_one_data') ) : the_row(); ?>
+              <tr>
+                <td class="<?php the_sub_field('row_status'); ?>">
+                  <?php the_sub_field('table_row'); ?>
+                </td>
+              </tr>
+            <?php
+            endwhile;
+          } ?>
+        </tbody>
+      </table>
+    </div>
 
-    <table class="<?php echo $classes; ?>">
-      <thead>
-        <tr>
-          <th>
-            <?php the_sub_field('table_two_heading'); ?>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        if( have_rows('table_two_data') ) {
-          while( have_rows('table_two_data') ) : the_row(); ?>
-            <tr>
-              <td>
-                <?php the_sub_field('table_row'); ?>
-              </td>
-            </tr>
+    <div class="<?php echo $classes; ?>">
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <?php the_sub_field('table_two_heading'); ?>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
           <?php
-          endwhile;
-        } ?>
-      </tbody>
-    </table>
+          if( have_rows('table_two_data') ) {
+            while( have_rows('table_two_data') ) : the_row(); ?>
+              <tr>
+                <td class="<?php the_sub_field('row_status'); ?>">
+                  <?php the_sub_field('table_row'); ?>
+                </td>
+              </tr>
+            <?php
+            endwhile;
+          } ?>
+        </tbody>
+      </table>
+    </div>
 
     <?php if( $number_of_tables == "three" ) { ?>
-    <table class="<?php echo $classes; ?>">
-      <thead>
-        <tr>
-          <th>
-            <?php the_sub_field('table_three_heading'); ?>
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <?php
-        if( have_rows('table_three_data') ) {
-          while( have_rows('table_three_data') ) : the_row(); ?>
-            <tr>
-              <td>
-                <?php the_sub_field('table_row'); ?>
-              </td>
-            </tr>
+    <div class="<?php echo $classes; ?>">
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <?php the_sub_field('table_three_heading'); ?>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
           <?php
-          endwhile;
-        } ?>
-      </tbody>
-    </table>
+          if( have_rows('table_three_data') ) {
+            while( have_rows('table_three_data') ) : the_row(); ?>
+              <tr>
+                <td class="<?php the_sub_field('row_status'); ?>">
+                  <?php the_sub_field('table_row'); ?>
+                </td>
+              </tr>
+            <?php
+            endwhile;
+          } ?>
+        </tbody>
+      </table>
+    </div>
     <?php
     }
   ?>
