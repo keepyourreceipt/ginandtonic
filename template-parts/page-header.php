f1f12064b937e1166931ae768dc5a9b727b35280
Michael <michaelkpeterman@gmail.com>
2016-02-03 18:47:39 -0500

    Install composer and require Twitter API Exchange

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -1,19 +1,21 @@
 <div class="page-header">
-  <?php $background_image = get_field('header_image'); ?>
-  <div class="jumbotron waypoint waypoint waypoint-fade anim-time-short" style="background-image: url(<?php echo $background_image['sizes']['full-hd'] ?>)">
-    <div class="image-overlay">
-      <?php // Image overlay ?>
-    </div>
-    <div class="container">
-      <div class="row">
-        <div class="table banner-text-container">
-          <div class="table-cell banner-text">
-            <?php if( get_field( 'text_heading' ) ) { ?>
-              <h1 class="waypoint waypoint-bottom-to-top"><?php the_field('text_heading'); ?></h1>
-              <?php } else { ?>
-              <h1 class="waypoint waypoint-bottom-to-top"><?php the_title(); ?></h1>
-            <?php } ?>
-            <h4 class="waypoint waypoint-bottom-to-top anim-time-medium"><?php the_field('text_sub_heading'); ?></h4>
+  <div class="container-fluid">
+    <?php $background_image = get_field('header_image'); ?>
+    <div class="row waypoint waypoint waypoint-fade" style="background-image: url(<?php echo $background_image['sizes']['full-hd'] ?>)">
+      <div class="image-overlay">
+        <?php // Image overlay ?>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="table banner-text-container">
+            <div class="table-cell banner-text">
+              <?php if( get_field( 'text_heading' ) ) { ?>
+                <h1 class="waypoint waypoint-bottom-to-top"><?php the_field('text_heading'); ?></h1>
+                <?php } else { ?>
+                <h1 class="waypoint waypoint-bottom-to-top"><?php the_title(); ?></h1>
+              <?php } ?>
+              <h4 class="waypoint waypoint-bottom-to-top anim-time-medium"><?php the_field('text_sub_heading'); ?></h4>
+            </div>
           </div>
         </div>
       </div>
