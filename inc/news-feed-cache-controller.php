1cdd13c52f89b096f38d1f52e350983228fa8af1
Michael <michaelkpeterman@gmail.com>
2016-02-14 17:27:39 -0500

    Fix news feed cache control logic

--- inc/news-feed-cache-controller.php
+++ inc/news-feed-cache-controller.php
@@ -1,13 +1,16 @@
 <?php
 
-$cache_expires = date("F j, Y, H:i", strtotime('+1 hour'));
+date_default_timezone_set("America/New_York");
 if ( file_exists( dirname(__FILE__) . '/news-feed.php' ) ) {
   $cache_file_uri = dirname(__FILE__) . '/news-feed.php';
   $cache_file_exists = true;
-  $cache_modified = date ("F d Y H:i:s.", filemtime($cache_file_uri));
+  $cache_modified = date ("F d Y H:i:s", filemtime($cache_file_uri));
+  $cache_expires = date( "F d Y H:i:s", strtotime($cache_modified . "+30 Minutes"));
+  $cache_access_time = date( "F d Y H:i:s" );
 
-  if( $cache_modified > $cache_expires ) {
+  if( strtotime( $cache_access_time ) > strtotime( $cache_expires ) ) {
     require_once dirname(__FILE__) . '/build-news-feed.php';
+  } else {
   }
 
 } else {
