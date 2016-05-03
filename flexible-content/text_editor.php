3cc55def015e419bfa05ea813fb2c86726178ab0
michaelkpeterman <michael@36creative.com>
2016-05-02 20:45:35 -0400

    Update padding classes Text Editor ACF

--- flexible-content/text_editor.php
+++ flexible-content/text_editor.php
@@ -1,9 +1,20 @@
 <?php
-  get_sub_field('padding_top') == "non-padded" ? $padding_classes .= " remove-padding-top" : $padding_classes .= " default-padding-top";
-  get_sub_field('padding_bottom') == "non-padded" ? $padding_classes .= " remove-padding-bottom" : $padding_classes .= " default-padding-bottom";
-  get_sub_field('content_width') == "full" ? $width_classes = "col-md-10 col-md-offset-1" : $width_classes = "col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2";
   $background_color = get_sub_field( 'background_color' );
   $text_color = get_sub_field( 'text_color' );
+
+  $padding_classes = ['padding-top' => 'default-padding-top', 'padding-bottom' => 'default-padding-bottom'];
+  if( get_sub_field('padding_top') == "non-padded" ) {
+    $padding_classes['padding-top'] = "remove-padding-top";
+  }
+  if( get_sub_field('padding_bottom') == "non-padded" ) {
+    $padding_classes['padding-bottom'] = "remove-padding-bottom";
+  }
+
+  if( get_sub_field( 'content_width' ) == "full" ) {
+    $width_classes = "col-md-10 col-md-offset-1";
+  } else {
+    $width_classes = "col-md-8 col-md-offset-2 col-lg-8 col-lg-offset-2";
+  }
 ?>
 <section class="text-editor" style="background-color: <?php echo $background_color; ?>; color: <?php echo $text_color; ?>;">
   <div class="container waypoint waypoint-bottom-to-top <?php echo $padding_classes; ?>">
