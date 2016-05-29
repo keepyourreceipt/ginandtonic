b6e156630d970798a56569dc8a145f30f8af1403
michaelkpeterman <michael@36creative.com>
2016-05-28 20:30:19 -0400

    Begin building tabbed content row

---
+++ flexible-content/product_features.php
@@ -0,0 +1,82 @@
+<section class="product-features">
+  <div class="container">
+    <div class="row">      
+      <div class="section-nav col-md-10 col-md-offset-1">
+        <?php if( get_sub_field( 'sec_one_section_title' ) ) { ?>
+          <button><?php the_sub_field( 'sec_one_section_title' ); ?></button>
+        <?php } ?>
+        <?php if( get_sub_field( 'sec_two_section_title' ) ) { ?>
+          <button><?php the_sub_field( 'sec_two_section_title' ); ?></button>
+        <?php } ?>
+      </div>
+      <div class="content-sections">
+        <?php // Section one ?>
+        <?php if( get_sub_field( 'sec_one_left_col_text_editor' ) || get_sub_field( 'sec_one_left_col_featured_image' ) ) { ?>
+          <div class="section-one col-md-10 col-md-offset-1">
+            <div class="row">
+              <div class="left-column col-md-6">
+                <?php
+                  if( get_sub_field( 'sec_one_left_col_content_type' ) == "text" ) {
+                    the_sub_field( 'sec_one_left_col_text_editor' );
+                  } else {
+                    $featured_image_obj = get_sub_field( 'sec_one_left_col_featured_image' );
+                    $featured_images_url = $featured_image_obj['sizes']['large'];
+                  ?>
+                  <div class="featured-image">
+                    <img src="<?php echo $featured_images_url; ?>" alt="Alt text here">
+                  </div>
+                <?php } ?>
+              </div>
+              <div class="right-column col-md-6">
+                <?php
+                  if( get_sub_field( 'sec_one_right_col_content_type' ) == "text" ) {
+                    the_sub_field( 'sec_one_right_col_text_editor' );
+                  } else {
+                    $featured_image_obj = get_sub_field( 'sec_one_right_col_featured_image' );
+                    $featured_images_url = $featured_image_obj['sizes']['large'];
+                  ?>
+                  <div class="featured-image">
+                    <img src="<?php echo $featured_images_url; ?>" alt="Alt text here">
+                  </div>
+                <?php } ?>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+        <?php // Section two ?>
+        <?php if( get_sub_field( 'sec_two_left_col_text_editor' ) || get_sub_field( 'sec_two_left_col_featured_image' ) ) { ?>
+          <div class="section-two col-md-10 col-md-offset-1">
+            <div class="row">
+              <div class="left-column col-md-6">
+                <?php
+                  if( get_sub_field( 'sec_two_left_col_content_type' ) == "text" ) {
+                    the_sub_field( 'sec_two_left_col_text_editor' );
+                  } else {
+                    $featured_image_obj = get_sub_field( 'sec_two_left_col_featured_image' );
+                    $featured_images_url = $featured_image_obj['sizes']['large'];
+                  ?>
+                  <div class="featured-image">
+                    <img src="<?php echo $featured_images_url; ?>" alt="Alt text here">
+                  </div>
+                <?php } ?>
+              </div>
+              <div class="right-column col-md-6">
+                <?php
+                  if( get_sub_field( 'sec_two_right_col_content_type' ) == "text" ) {
+                    the_sub_field( 'sec_two_right_col_text_editor' );
+                  } else {
+                    $featured_image_obj = get_sub_field( 'sec_two_right_col_featured_image' );
+                    $featured_images_url = $featured_image_obj['sizes']['large'];
+                  ?>
+                  <div class="featured-image">
+                    <img src="<?php echo $featured_images_url; ?>" alt="Alt text here">
+                  </div>
+                <?php } ?>
+              </div>
+            </div>
+          </div>
+        <?php } ?>
+      </div>
+    </div>
+  </div>
+</section>
