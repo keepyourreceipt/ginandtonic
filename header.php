67bc6ab07b2e74fa0d52a185f713fd14e52b6b25
michaelkpeterman <michael@36creative.com>
2016-05-30 18:36:50 -0400

    Remove meta tag SEO fix

--- header.php
+++ header.php
@@ -3,8 +3,7 @@
   <head>
       <meta charset="utf-8">
       <meta http-equiv="x-ua-compatible" content="ie=edge">
-      <title><?php wp_title(); ?></title>
-      <meta name="description" content="">
+      <title><?php wp_title(); ?></title>      
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <?php // Google varification ?>
       <meta name="google-site-verification" content="uTilvixm63uU6mVTpR7BMbwYvflF86eftiwR7z_83As" />
