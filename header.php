ac52e62bd4fb8b91ac51e3ff7ec07902a4f85a59
michaelkpeterman <michael@36creative.com>
2016-05-30 23:04:20 -0400

    Add favicon. Style updates

--- header.php
+++ header.php
@@ -3,11 +3,13 @@
   <head>
       <meta charset="utf-8">
       <meta http-equiv="x-ua-compatible" content="ie=edge">
-      <title><?php wp_title(); ?></title>      
+      <title><?php wp_title(); ?></title>
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <?php // Google varification ?>
       <meta name="google-site-verification" content="uTilvixm63uU6mVTpR7BMbwYvflF86eftiwR7z_83As" />
       <?php get_template_part( 'template', 'parts/custom-font-scripts' ); ?>
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+      <link rel="icon" href="/favicon.ico" type="image/x-icon">
       <?php wp_head(); ?>
   <script
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAbuyLNGCjqB0wn5NG-ZInssrccSlY28IY">
